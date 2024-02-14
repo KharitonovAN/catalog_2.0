7a6e64a2fd9b99f42c5fee9bfae47496b641540d
HAN <han82@mail.ru>
2024-02-14 03:50:32 +0300

    22,2 in work

--- blogpost/models.py
+++ blogpost/models.py
@@ -4,12 +4,12 @@ NULLABLE = {'null': True, 'blank': True}
 
 
 class BlogPost(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, verbose_name='Название')
     slug = models.CharField(max_length=150, unique=True, **NULLABLE)
-    content = models.TextField(**NULLABLE)
-    preview_image = models.ImageField(upload_to='blogpost/images/', **NULLABLE)
+    content = models.TextField(**NULLABLE, verbose_name='Текст')
+    preview_image = models.ImageField(upload_to='blogpost/images/', **NULLABLE, verbose_name='Изображение')
     created_at = models.DateTimeField(auto_now_add=True)
-    is_published = models.BooleanField(default=False)
+    is_published = models.BooleanField(default=False, verbose_name='В публикации')
     views = models.PositiveIntegerField(default=0)
 
     def __str__(self):
